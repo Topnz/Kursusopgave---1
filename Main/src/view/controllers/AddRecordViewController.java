cb526e026b498122456b1ebd06be8984c6d49cdf
Rumeysa <202310247@post.au.dk>
2026-02-27 14:17:49 +0100

    Commit

--- Main/src/view/controllers/AddRecordViewController.java
+++ Main/src/view/controllers/AddRecordViewController.java
@@ -1,5 +1,66 @@
 package view.controllers;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.Region;
+import view.ViewHandler;
+import viewmodel.AddRecordViewModel;
+import javafx.event.ActionEvent;
+
 public class AddRecordViewController
 {
+
+  @FXML private TextField addRecordArtistTextField;
+  @FXML private TextField addRecordTitleTextField;
+  @FXML private TextField addRecordReleaseYearTextField;
+
+  private ViewHandler viewHandler;
+  private Region root;
+  private AddRecordViewModel addRecordViewModel;
+
+  public AddRecordViewController()
+  {
+    // Called by FXML Loader
+  }
+  public void init(ViewHandler viewHandler,
+      AddRecordViewModel addRecordViewModel,
+      Region root)
+  {
+    this.viewHandler = viewHandler;
+    this.addRecordViewModel = addRecordViewModel;
+    this.root = root;
+
+    addRecordArtistTextField.textProperty().bindBidirectional(addRecordViewModel.getAddRecordArtistProperty());
+    addRecordTitleTextField.textProperty().bindBidirectional(addRecordViewModel.getAddRecordTitleProperty());
+    addRecordReleaseYearTextField.textProperty().bindBidirectional(addRecordViewModel.getAddRecordReleaseYearProperty());
+
+    reset();
+  }
+
+  public void reset()
+  {
+    addRecordViewModel.clear();
+  }
+
+  public Region getRoot()
+  {
+    return root;
+  }
+
+  @FXML
+  private void actionButtonPressed()
+  {
+      boolean addRecord = addRecordViewModel.addRecord();
+      if (addRecord)
+      {
+        viewHandler.openView("Overview");
+      }
+  }
+
+  @FXML
+  private void backButtonPressed()
+  {
+    viewHandler.openView("overview");
+  }
 }
--- Main/src/view/controllers/OverviewViewController.java
+++ Main/src/view/controllers/OverviewViewController.java
@@ -1,5 +1,64 @@
 package view.controllers;
 
+import javafx.fxml.FXML;
+import javafx.scene.layout.Region;
+import view.ViewHandler;
+
+import javax.swing.table.TableColumn;
+
 public class OverviewViewController
 {
+
+  @FXML private TableColumn<OverViewViewModel, String> tableTitle;
+  @FXML private TableColumn<OverViewViewModel, String> tableArtist;
+  @FXML private TableColumn<OverViewViewModel, String> tableReleaseDate;
+  @FXML private TableColumn<OverViewViewModel, String> tableRentedStatus;
+  private Region root;
+
+  private ViewHandler viewHandler;
+  private Region root;
+  private OverViewViewModel overViewViewModel;
+
+  OverviewViewController()
+  {
+    //called by FXML loader
+  }
+  public void init(ViewHandler viewHandler, OverViewModel overViewModel,
+      Region root)
+  {
+    this.viewHandler = viewHandler;
+    this.overViewViewModel = overViewViewModel;
+    this.root = root;
+
+    tableTitle.setCellValueFactory(cellData -> cellData.getValue().
+
+        getTableTitleProperty());
+
+    tableArtist.setCellValueFactory(cellData -> cellData.getValue().
+
+        getTableArtistProperty());
+
+    tableReleaseDate.setCellValueFactory(cellData -> cellData.getValue().
+
+        getTableReleaseDateProperty());
+
+    tableRentedStatus.setCellValueFactory(cellData -> cellData.getValue().
+
+        getTableRentedStatusProperty());
+
+    reset();
+  }
+
+
+  public void reset()
+  {
+    OverViewViewModel.clear();
+  }
+
+  public Region getRoot()
+  {
+    return root;
+  }
+
+
 }
